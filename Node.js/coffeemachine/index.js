73a3b470adcba271e49c89851936aaac63382629
Gilbert Hutapea <berthutapea@gmail.com>
2023-01-21 21:54:02 +0700

    Penambahan Module Node js

---
+++ Node.js/coffeemachine/index.js
@@ -0,0 +1,32 @@
+// import coffeeStock from "./state.js";
+import { coffeeStock as stock, isCoffeeMachineReady } from './state.js';
+
+// const displayStock = stock => {
+//     for (const type in stock) {
+//         console.log(type);
+//     }
+// }
+
+// displayStock(coffeeStock);
+// const { coffeeStock, isCoffeeMachineReady } = require('./state');
+
+// console.log(coffeeStock);
+console.log(stock);
+console.log(isCoffeeMachineReady);
+// console.log(coffeeStock);
+// const makeCoffee = (type, miligrams) => {
+//     if (coffeeStock[type] >= miligrams) {
+//         console.log("Kopi berhasil dibuat!");
+//     } else {
+//         console.log("Biji kopi habis!");
+//     }
+// }
+
+// makeCoffee("robusta", 80);
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan air");
+// console.log("Mencampurkan air dan kopi");
+// console.log("Menuangkan kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah siap!");
---
+++ Node.js/coffeemachine/state.js
@@ -0,0 +1,14 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+};
+
+const isCoffeeMachineReady = true;
+ 
+export {coffeeStock, isCoffeeMachineReady};
+
+// export default coffeeStock;
+// const isCoffeeMachineReady = true;
+// module.exports = {coffeeStock, isCoffeeMachineReady}; 
+// console.log(module);
