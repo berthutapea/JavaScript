bc505b721255809994f82cd04ad8fa66cf60cc37
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-01-26 14:24:35 +0700

    Pemanmbahan Lodash

---
+++ Node.js/play-with-npm/index.js
@@ -0,0 +1,27 @@
+// import variableName from 'play-with-npm';
+import _ from 'lodash';
+/* 
+Fungsi Reduce
+*/
+// const myArray = [1, 2, 3, 4];
+// let sum = myArray.reduce((prev, curr) => {
+//     return prev + curr;
+// });
+ 
+// console.log(sum);
+ 
+/* output
+10
+*/
+
+/* 
+Lodash
+*/
+// const myArray = [1, 2, 3, 4];
+// const sum = _.sum(myArray);
+ 
+// console.log(sum);
+ 
+/* output
+10
+*/
